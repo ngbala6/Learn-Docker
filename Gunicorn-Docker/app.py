dcd5ca647a5646fe0c7f3ff49f13d201805333f1
Bala Murugan N G <43375713+ngbala@users.noreply.github.com>
2020-02-18 17:58:23 +0530

    Updated app.py

--- Gunicorn-Docker/app.py
+++ Gunicorn-Docker/app.py
@@ -1,3 +1,5 @@
+# @author
+# Bala Murugan N G
 from flask import Flask
 import os
 
@@ -20,3 +22,6 @@ if __name__ == '__main__':
 		app.run(host='0.0.0.0', port=5000, debug=False)
 	else:
 		app.run(host='0.0.0.0', port=int(cf_port), debug=False)
+
+# @author
+# Bala Murugan N G
